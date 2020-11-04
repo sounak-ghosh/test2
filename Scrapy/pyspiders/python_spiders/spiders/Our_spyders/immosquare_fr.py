ddfdc05b041fbcdfc9e9c5a01a6c0e215ca581e5
surajpasi <kscan@karza.in>
2020-11-04 10:35:40 +0530

    immosquare_fr && regiesaintlouis_fr

---
+++ Scrapy/pyspiders/python_spiders/spiders/Our_spyders/immosquare_fr.py
@@ -0,0 +1,282 @@
+import scrapy
+import js2xml
+from ..loaders import ListingLoader
+from ..items import ListingItem
+from python_spiders.helper import remove_unicode_char, extract_rent_currency, format_date
+import re,json
+from bs4 import BeautifulSoup
+import requests
+import geopy
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="myGeocoder")
+
+def get_lat_lon(_address):
+    location = geolocator.geocode(_address)
+    return location.latitude,location.longitude
+
+
+def getAddress(lat,lng):
+    coordinates = str(lat)+","+str(lng)
+    location = geolocator.reverse(coordinates)
+    return location
+
+def getSqureMtr(text):
+    list_text = re.findall(r'\d+',text)
+
+    if len(list_text) == 3:
+        output = float(list_text[0]+"."+list_text[1])
+    elif len(list_text) == 2:
+        output = float(list_text[0]+"."+list_text[1])
+    elif len(list_text) == 1:
+        output = int(list_text[0])
+    else:
+        output=0
+
+    return int(output)
+
+def getPrice(text):
+    list_text = re.findall(r'\d+',text)
+
+    if len(list_text) == 2:
+        output = float(list_text[0]+list_text[1])
+    elif len(list_text) == 1:
+        output = int(list_text[0])
+    else:
+        output=0
+
+    return int(output)
+
+
+def cleanText(text):
+    text = ''.join(text.split())
+    text = re.sub(r'[^a-zA-Z0-9]', ' ', text).strip()
+    return text.replace(" ","_").lower()
+
+
+def num_there(s):
+    return any(i.isdigit() for i in s)
+
+
+def cleanKey(data):
+    if isinstance(data,dict):
+        dic = {}
+        for k,v in data.items():
+            dic[cleanText(k)]=cleanKey(v)
+        return dic
+    else:
+        return data
+
+
+def clean_value(text):
+    if text is None:
+        text = ""
+    if isinstance(text,(int,float)):
+        text = str(text.encode('utf-8').decode('ascii', 'ignore'))
+    text = str(text.encode('utf-8').decode('ascii', 'ignore'))
+    text = text.replace('\t','').replace('\r','').replace('\n','')
+    return text.strip()
+
+def clean_key(text):
+    if isinstance(text,str):
+        text = ''.join([i if ord(i) < 128 else ' ' for i in text])
+        text = text.lower()
+        text = ''.join([c if 97 <= ord(c) <= 122 or 48 <= ord(c) <= 57 else '_'                                                                                         for c in text ])
+        text = re.sub(r'_{1,}', '_', text)
+        text = text.strip("_")
+        text = text.strip()
+
+        if not text:
+            raise Exception("make_key :: Blank Key after Cleaning")
+
+        return text.lower()
+    else:
+        raise Exception("make_key :: Found invalid type, required str or unicode                                                                                        ")
+
+def traverse( data):
+    if isinstance(data, dict):
+        n = {}
+        for k, v in data.items():
+            k = str(k)
+            if k.startswith("dflag") or k.startswith("kflag"):
+                if k.startswith("dflag_dev") == False:
+                    n[k] = v
+                    continue
+
+            n[clean_key(clean_value(k))] = traverse(v)
+
+        return n
+
+    elif isinstance(data, list) or isinstance(data, tuple) or isinstance(data, set):                                                                                     
+        data = list(data)
+        for i, v in enumerate(data):
+            data[i] = traverse(v)
+
+        return data
+    elif data is None:
+        return ""
+    else:
+        data = clean_value(data)
+        return data
+
+class QuotesSpider(scrapy.Spider):
+    name = 'immosquare_fr'
+    allowed_domains = ['www.immosquare.fr']
+    start_urls = ['www.immosquare.fr']
+    execution_type = 'testing'
+    country = 'french'
+    locale ='fr'
+
+    def start_requests(self):
+        start_urls = [{"url":"https://www.immosquare.fr/immobilier-annonce.html"}]
+
+        for urls in start_urls:
+            yield scrapy.Request(
+                url=urls.get('url'),
+                callback=self.parse,
+                meta = {"url":urls.get("url")}
+                )
+
+
+
+    def parse(self, response, **kwargs):
+        soup = BeautifulSoup(response.body,"html.parser")
+        
+        if soup.find("ul",id="itemContainer"):
+            lis_property= soup.find("ul",id="itemContainer").find_all("li",class_="annonce mod mr1 mb1 left")
+
+        for ech_li in lis_property:
+            
+            text_url = ech_li.find("a")["href"]
+            external_link = "https://www.immosquare.fr/"+text_url
+            price = getPrice(ech_li.find("span").text)
+            city = ech_li.find("h5").find("a").text
+
+            yield scrapy.Request(
+                url=external_link,
+                callback=self.get_property_details,
+                meta = {"city":city,"rent":price}
+                )
+
+       
+    def get_property_details(self, response, **kwargs):
+        item = ListingItem()
+        soup = BeautifulSoup(response.body,"html.parser")
+        str_soup = str(soup)
+
+        print (response.url)
+
+        match = re.findall("address:(.+),",str_soup)
+        if match:
+            address = (match)[0].strip('"').strip()
+            try:
+                latitude,longitude = get_lat_lon(address)
+                location = getAddress(latitude,longitude)
+
+                item["zipcode"] = location.raw["address"]["postcode"]
+                item["latitude"] = str(latitude)
+                item["longitude"] = str(longitude)
+            except:
+                pass
+            item["address"] = address
+
+
+
+        title = soup.find("h1",class_="rouge").text.strip()
+        item["title"] = title
+        if "tudiant" in title.lower() or  "studenten" in title.lower() and "appartement" in title.lower():
+            property_type = "student_apartment"
+        elif "appartement" in title.lower():
+            property_type = "apartment"
+        elif "woning" in title.lower() or "maison" in title.lower() or "huis" in title.lower() or "duplex" in title.lower():
+            property_type = "house"
+        elif "chambre" in title.lower() or "kamer" in title.lower():
+            property_type = "room"
+        elif "studio" in title.lower():
+            property_type = "studio"
+        else:
+            property_type = "NA"
+
+
+
+        description = soup.find("div",class_="mod w50 right pl2").text.strip()
+        item["description"] = description
+
+        if "garage" in description.lower() or "parking" in description.lower() or "autostaanplaat" in description.lower():
+            item["parking"]=True
+        if "terras" in description.lower() or "terrace" in description.lower():
+            item["terrace"]=True
+        if "balcon" in description.lower() or "balcony" in description.lower():
+            item["balcony"]=True
+        if "zwembad" in description.lower() or "swimming" in description.lower():
+            item["swimming_pool"]=True
+        if "gemeubileerd" in description.lower() or "furnished" in description.lower():
+            item["furnished"]=True
+        if "machine à laver" in description.lower():
+            item["washing_machine"]=True
+        if "lave" in description.lower() and "vaisselle" in description.lower():
+            item["dishwasher"]=True
+        if "lift" in description.lower():
+            item["elevator"]=True
+
+
+
+        img_lst=[]
+        if soup.find("div",class_="royalSlider rsDefault"):
+            img = soup.find("div",class_="royalSlider rsDefault").find_all("a",class_="rsImg")
+            for items in img:
+                img_lst.append(items["href"])
+
+            item["images"] = img_lst
+            item["external_images_count"] = len(img_lst)
+
+
+        temp_dic = {}
+        gen_dts = soup.find("div",class_="line grid4").find_all("li")
+        for items in gen_dts:
+            text = items.text.split(":")
+            temp_dic[text[0].strip()] = text[1].strip()
+
+        temp_dic = cleanKey(temp_dic)
+        if "pi_ces" in temp_dic:
+            item["room_count"] = getSqureMtr(temp_dic["pi_ces"])
+        if "surfacehabitable" in temp_dic and getSqureMtr(temp_dic["surfacehabitable"]):
+            item["square_meters"] = getSqureMtr(temp_dic["surfacehabitable"])
+        if "salled_eau" in temp_dic and getSqureMtr(temp_dic["salled_eau"]):
+            item["bathroom_count"] = getSqureMtr(temp_dic["salled_eau"])
+        if "salledebains" in temp_dic and getSqureMtr(temp_dic["salledebains"]):
+            item["bathroom_count"] = getSqureMtr(temp_dic["salledebains"])
+        if "parkingext_rieur" in temp_dic:
+            item["parking"] = True
+        if "terrasse" in temp_dic:
+            item["terrace"] = True
+
+
+        if soup.find("div",class_="mod w50 left consommation mt2 mb2"):
+            energy = soup.find("div",class_="mod w50 left consommation mt2 mb2").find("p").text
+            eng = energy.replace("Consommationénergétique","")
+            eng_value = eng.split("/")[0].replace("kWh","")
+            if int(eng_value.strip()) != 0:
+                item["energy_label"] = eng
+
+        if soup.find("div",class_="widget mod w30 left pl3"):
+            phone_num = soup.find("div",class_="widget mod w30 left pl3").find("span").text.strip()
+            name = soup.find("div",class_="widget mod w30 left pl3").find("p",class_="h3").text.strip().replace(phone_num,"").strip()
+
+            item["landlord_name"] = name
+            item["landlord_phone"] = phone_num
+
+
+
+
+        item["currency"] = "EUR"
+        item["external_source"] = "immosquare.fr"
+        item["external_link"] = response.url
+        item["city"] = response.meta["city"]
+        item["rent"] = response.meta["rent"]
+
+
+        if property_type in ["apartment", "house", "room", "property_for_sale", "student_apartment", "studio"]:
+            item["property_type"] = property_type
+            print (item)
+            yield item
---
+++ Scrapy/pyspiders/python_spiders/spiders/Our_spyders/regiesaintlouis_fr.py
@@ -0,0 +1,335 @@
+import scrapy
+import js2xml
+from ..loaders import ListingLoader
+from ..items import ListingItem
+from python_spiders.helper import remove_unicode_char, extract_rent_currency, format_date
+import re,json
+from bs4 import BeautifulSoup
+import requests
+import geopy
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="myGeocoder")
+
+def get_lat_lon(_address):
+    location = geolocator.geocode(_address)
+    return location.latitude,location.longitude
+
+
+def getAddress(lat,lng):
+    coordinates = str(lat)+","+str(lng)
+    location = geolocator.reverse(coordinates)
+    return location
+
+def getSqureMtr(text):
+    list_text = re.findall(r'\d+',text)
+
+    if len(list_text) == 3:
+        output = float(list_text[0]+"."+list_text[1])
+    elif len(list_text) == 2:
+        output = float(list_text[0]+"."+list_text[1])
+    elif len(list_text) == 1:
+        output = int(list_text[0])
+    else:
+        output=0
+
+    return int(output)
+
+def getPrice(text):
+    list_text = re.findall(r'\d+',text)
+
+    if len(list_text) == 2:
+        output = float(list_text[0]+list_text[1])
+    elif len(list_text) == 1:
+        output = int(list_text[0])
+    else:
+        output=0
+
+    return int(output)
+
+
+def cleanText(text):
+    text = ''.join(text.split())
+    text = re.sub(r'[^a-zA-Z0-9]', ' ', text).strip()
+    return text.replace(" ","_").lower()
+
+
+def num_there(s):
+    return any(i.isdigit() for i in s)
+
+
+def cleanKey(data):
+    if isinstance(data,dict):
+        dic = {}
+        for k,v in data.items():
+            dic[cleanText(k)]=cleanKey(v)
+        return dic
+    else:
+        return data
+
+
+def clean_value(text):
+    if text is None:
+        text = ""
+    if isinstance(text,(int,float)):
+        text = str(text.encode('utf-8').decode('ascii', 'ignore'))
+    text = str(text.encode('utf-8').decode('ascii', 'ignore'))
+    text = text.replace('\t','').replace('\r','').replace('\n','')
+    return text.strip()
+
+def clean_key(text):
+    if isinstance(text,str):
+        text = ''.join([i if ord(i) < 128 else ' ' for i in text])
+        text = text.lower()
+        text = ''.join([c if 97 <= ord(c) <= 122 or 48 <= ord(c) <= 57 else '_'                                                                                         for c in text ])
+        text = re.sub(r'_{1,}', '_', text)
+        text = text.strip("_")
+        text = text.strip()
+
+        if not text:
+            raise Exception("make_key :: Blank Key after Cleaning")
+
+        return text.lower()
+    else:
+        raise Exception("make_key :: Found invalid type, required str or unicode                                                                                        ")
+
+def traverse( data):
+    if isinstance(data, dict):
+        n = {}
+        for k, v in data.items():
+            k = str(k)
+            if k.startswith("dflag") or k.startswith("kflag"):
+                if k.startswith("dflag_dev") == False:
+                    n[k] = v
+                    continue
+
+            n[clean_key(clean_value(k))] = traverse(v)
+
+        return n
+
+    elif isinstance(data, list) or isinstance(data, tuple) or isinstance(data, set):                                                                                     
+        data = list(data)
+        for i, v in enumerate(data):
+            data[i] = traverse(v)
+
+        return data
+    elif data is None:
+        return ""
+    else:
+        data = clean_value(data)
+        return data
+
+class QuotesSpider(scrapy.Spider):
+    name = 'regiesaintlouis_fr'
+    allowed_domains = ['www.regiesaintlouis.com']
+    start_urls = ['www.regiesaintlouis.com']
+    execution_type = 'testing'
+    country = 'french'
+    locale ='fr'
+
+    def start_requests(self):
+        url="http://www.regiesaintlouis.com/fr/recherche/"
+
+        property_value = ["1","2","6"]
+        for p_t in property_value:
+            data={"nature": "2","type[]": p_t,"price": "","reference": "", 
+            "age": "","tenant_min": "","tenant_max": "","rent_type": "", "currency": "EUR"}
+
+            if "1" ==  p_t or "6" == p_t: 
+                property_type = "apartment"
+            elif "2" == p_t:
+                property_type = "house"
+
+            yield scrapy.FormRequest(
+                url=url,
+                formdata = data,
+                callback=self.parse,
+                meta = {"property_type":property_type}
+                )
+
+
+    def parse(self, response, **kwargs):
+        soup = BeautifulSoup(response.body,"html.parser")
+
+        property_type = response.meta["property_type"]
+        if soup.find("ul", class_="pager"):
+            all_pages = soup.find("ul", class_="pager").find_all("li")
+            if num_there(all_pages[-1].text):
+                count = int(all_pages[-1].text)
+            elif num_there(all_pages[-2].text):
+                count = int(all_pages[-2].text)
+            elif num_there(all_pages[-3].text):
+                count = int(all_pages[-3].text)
+
+            for c in range(count):
+                url = "http://www.regiesaintlouis.com/fr/recherche/"+str(c+1)
+                yield scrapy.Request(
+                    url=url,
+                    callback=self.get_page_details,
+                    meta = {"property_type":property_type}
+                    )
+
+
+
+    def get_page_details(self, response, **kwargs):
+        soup = BeautifulSoup(response.body,"html.parser")
+
+        property_type = response.meta["property_type"]
+        articles = soup.find("ul",class_="ads").find_all("li")
+        for ech_art in articles:
+            external_link = "http://www.regiesaintlouis.com"+ech_art.find("a")["href"]
+            print (external_link)
+            yield scrapy.Request(
+                url=external_link,
+                callback=self.get_property_details,
+                meta = {"property_type":property_type}
+                )
+
+       
+    def get_property_details(self, response, **kwargs):
+        item = ListingItem()
+        soup = BeautifulSoup(response.body,"html.parser")
+        str_soup = str(soup)
+
+        print (response.url)
+
+        match = re.findall("= L.marker(.+);",str_soup)
+        if match:
+            lat_lng = eval(match[0].split(",{")[0].replace("(",""))
+            latitude = str(lat_lng[0])
+            longitude = str(lat_lng[1])
+
+            location = getAddress(latitude,longitude)
+            item["address"] = location.address
+            item["latitude"] = latitude
+            item["longitude"] = longitude
+
+            if "city" in location.raw["address"]:
+                item["city"] = location.raw["address"]["city"]
+            elif "town" in location.raw["address"]:
+                item["city"]= location.raw["address"]["town"]
+            if "postcode" in location.raw["address"]:
+                item["zipcode"] = location.raw["address"]["postcode"]
+
+
+
+        title = soup.find("div",class_="title").find("h1").text.strip()
+        item["title"] = title
+        rent = getSqureMtr(soup.find("div",class_="title").find("h2").text.strip())
+        item["rent"] = rent
+
+        ref =getSqureMtr(soup.find("section", class_="main show").find("article").find("p",class_="comment").find("span",class_="reference").text)
+        item["external_id"]=str(ref)
+
+
+
+
+        desc = soup.find("section", class_="main show").find("article").find("p",class_="comment").text.strip()
+        item["description"] = desc
+
+        if "garage" in desc.lower() or "parking" in desc.lower() or "autostaanplaat" in desc.lower():
+            item["parking"] = True
+        if "terras" in desc.lower() or "terrace" in desc.lower():
+            item["terrace"] = True
+        if "balcon" in desc.lower() or "balcony" in desc.lower():
+            item["balcony"] = True
+        if "zwembad" in desc.lower() or "swimming" in desc.lower():
+            item["swimming_pool"] = True
+        if "gemeubileerd" in desc.lower() or "furnished" in desc.lower() or "meublé" in desc.lower():
+            item["furnished"] = True
+        if "machine à laver" in desc.lower():
+            item["washing_machine"] = True
+        if "lave" in desc.lower() and "vaisselle" in desc.lower():
+            item["dishwasher"] = True
+        if "lift" in desc.lower() or "ascenseur" in desc.lower():
+            item["elevator"] = True
+
+
+
+
+        detail_head = []
+        all_head = soup.find("div", class_="details clearfix").find_all("div")
+        for ech_head in all_head:
+            detail_head.append(ech_head.find("h3").text.strip().lower())
+
+        temp_dic = {}
+        if "résumé" in detail_head:
+            summary = soup.find("div", class_="details clearfix").find("div",class_="summary").find_all("li")
+            for ech_li in summary:
+                temp_dic[ech_li.text.replace(ech_li.find("span").text,"").strip()]=ech_li.find("span").text.strip()
+
+            temp_dic = cleanKey(temp_dic)
+            if "pi_ces" in temp_dic and getSqureMtr(temp_dic["pi_ces"]):
+                item["room_count"] = getSqureMtr(temp_dic["pi_ces"])
+            if "surface" in temp_dic and getSqureMtr(temp_dic["surface"]):
+                item["square_meters"] = getSqureMtr(temp_dic["surface"])
+            if "etage" in temp_dic:
+                item["floor"] = temp_dic["etage"]
+            if "disponiblele" in temp_dic:
+                if num_there(temp_dic["disponiblele"]):
+                    date = temp_dic["disponiblele"]+"20"
+                    item["available_date"]=format_date(date)
+
+
+        if "informations légales" in detail_head:
+            legal = soup.find("div", class_="details clearfix").find("div", class_="legal").find_all("li")
+            temp_dic1 = {}
+            for ech_li in legal:
+                if ech_li.find("span"):
+                    temp_dic[ech_li.text.replace(ech_li.find("span").text,"").strip()] = ech_li.find("span").text
+            temp_dic1 = cleanKey(temp_dic1)
+
+            if "charges" in temp_dic and getPrice(temp_dic["charges"]):
+                item["utilities"]=getPrice(temp_dic["charges"])
+            if "d_p_tdegarantie" in temp_dic and getPrice(temp_dic["d_p_tdegarantie"]):
+                item["deposit"]=getPrice(temp_dic["d_p_tdegarantie"])
+
+
+        if "efficacité énergétique" in detail_head:
+            energy = soup.find("div", class_="diagnostics").find("img",alt="Énergie - Consommation conventionnelle")["src"].split("/")[-1]
+            energy = energy.replace("%2C",".")
+            item["energy_label"] = energy+" kWhEP/m².an"
+
+
+        if "prestations" in detail_head:
+            services = soup.find("div",class_="services").find_all("li")
+            for ech_ser in services:
+                facilities= ech_ser.text.strip()
+                if "garage" in facilities.lower() or "parking" in facilities.lower() or "autostaanplaat" in facilities.lower():
+                    item["parking"] = True
+                if "terras" in facilities.lower() or "terrace" in facilities.lower():
+                    item["terrace"] = True
+                if "balcon" in facilities.lower() or "balcony" in facilities.lower():
+                    item["balcony"] = True
+                if "zwembad" in facilities.lower() or "swimming" in facilities.lower():
+                    item["swimming_pool"] = True
+                if "gemeubileerd" in facilities.lower() or "furnished" in facilities.lower() or "meublé" in facilities.lower():
+                    item["furnished"] = True
+                if "machine à laver" in facilities.lower():
+                    item["washing_machine"] = True
+                if "lave" in facilities.lower() and "vaisselle" in facilities.lower():
+                    item["dishwasher"] = True
+                if "lift" in facilities.lower() or "ascenseur" in facilities.lower():
+                    item["elevator"] = True
+
+
+
+        images_list = []
+        if soup.find("div", class_="show-carousel owl-carousel owl-theme"):
+            imgs = soup.find("div", class_="show-carousel owl-carousel owl-theme").find_all("div", class_="item resizePicture")
+            for im in imgs:
+                images_list.append(im.find("a")["href"])
+
+            if images_list:
+                item["images"] = images_list
+                item["external_images_count"] = len(images_list)
+
+        item["landlord_name"] = "Régie Saint-Louis"
+        item["external_source"] = "regiesaintlouis.com"
+        item["currency"] = "EUR"
+        item["landlord_email"]="location@saintlouis.immo"
+        item["landlord_phone"] ="+33472845205" 
+        item["property_type"] = response.meta["property_type"]
+        item["external_link"] = response.url
+        print (item)
+        yield item
